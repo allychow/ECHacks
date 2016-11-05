dd54d4a93f0b2c02f95cae495eee0795e1f247cc
Allison Chow <allison.chow@outlook.com>
2016-11-05 11:13:07 -0400

    Add files via upload

---
+++ SyncAdapter.java
@@ -0,0 +1,68 @@
+package com.example.andrewseto.echacks;
+import android.content.Intent;
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.ContentResolver;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.Context;
+import android.content.SyncResult;
+import android.content.ContentProviderClient;
+import android.accounts.Account;
+
+/**
+ * Created by andrewseto on 05/11/2016.
+ */
+public class SyncAdapter extends AbstractThreadedSyncAdapter {
+
+    // Global variables
+    // Define a variable to contain a content resolver instance
+    ContentResolver mContentResolver;
+
+    /**
+     * Set up the sync adapter
+     */
+    public SyncAdapter(Context context, boolean autoInitialize) {
+        super(context, autoInitialize);
+        /*
+         * If your app uses a content resolver, get an instance of it
+         * from the incoming Context
+         */
+        mContentResolver = context.getContentResolver();
+    }
+
+    public SyncAdapter(
+            Context context,
+            boolean autoInitialize,
+            boolean allowParallelSyncs) {
+        super(context, autoInitialize, allowParallelSyncs);
+        /*
+         * If your app uses a content resolver, get an instance of it
+         * from the incoming Context
+         */
+        mContentResolver = context.getContentResolver();
+
+    }
+
+    @Override
+    public void onPerformSync(
+            Account account,
+            Bundle extras,
+            String authority,
+            ContentProviderClient provider,
+            SyncResult syncResult) {
+    /*
+     * Put the data transfer code here.
+     */
+
+    }
+}
